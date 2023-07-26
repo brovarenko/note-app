c1d1b71409ec9d5743cc7260febcd5d0ea218fd2
vladyslav-brovarenko <brovwg@gmail.com>
2023-07-26 19:41:05 +0300

    added form for add note

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let notes = [
     archived: false,
   },
   {
-    id: 1,
+    id: 2,
     name: 'Books',
     createdAt: '2023-07-25 12:00',
     content: 'Buy book',
@@ -58,16 +58,6 @@ function renderNotes() {
 }
 
 function handleAddNote() {
-  const newNote = {
-    id: 1,
-    name: 'Books',
-    createdAt: '2023-07-25 12:00',
-    content: 'Buy book',
-    category: 'Task',
-    dates: ['2023-06-20', '2023-08-20'],
-    archived: false,
-  };
-
   notes.push(newNote);
 
   renderNotes();
@@ -82,6 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function handleDeleteNote(event) {
   const noteId = parseInt(event.target.dataset.noteId);
+  console.log(event.target.dataset);
   try {
     const noteIndex = notes.findIndex((note) => note.id === noteId);
     if (noteIndex !== -1) {
@@ -91,4 +82,79 @@ function handleDeleteNote(event) {
   } catch (error) {
     console.error(error.message);
   }
+}
+
+let formContainer = null;
+
+function showAddNoteForm() {
+  if (formContainer) {
+    formContainer.remove();
+    formContainer = null;
+    return;
+  }
+  formContainer = document.createElement('div');
+
+  formContainer.classList.add('add-form');
+
+  const form = document.createElement('form');
+  form.innerHTML = `
+    <label for="name">Name:</label>
+    <input id="content" name="name" required></input>
+
+    <label for="content">Content:</label>
+    <textarea id="content" name="content" rows="3" required></textarea>
+
+    <label for="category">Category:</label>
+    <select id="category" name="category" required>
+      <option value="Task">Task</option>
+      <option value="Random Thought">Random Thought</option>
+      <option value="Idea">Idea</option>
+    </select>
+
+
+    <button type="submit">Add</button>
+  `;
+
+  form.addEventListener('submit', handleFormSubmit);
+
+  formContainer.appendChild(form);
+
+  const addButton = document.querySelector('#addButton');
+
+  addButton.insertAdjacentElement('afterend', formContainer);
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const formData = new FormData(form);
+  const contentDates =
+    formData.get('content').match(/\b\d{1,2}[\.\/]\d{1,2}[\.\/]\d{4}\b/g) || [];
+
+  const newNote = {
+    id: notes.length + 1,
+    name: formData.get('name'),
+    createdAt: formatDate(new Date().toISOString()),
+    content: formData.get('content'),
+    category: formData.get('category'),
+    dates: contentDates,
+    archived: false,
+  };
+
+  notes.push(newNote);
+
+  form.parentElement.remove();
+  formContainer = null;
+  renderNotes();
+}
+
+function handleAddNote() {
+  showAddNoteForm();
 }
